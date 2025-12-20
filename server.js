8e88d408b72439ae4c98020cc09521777113ca8b
Von Matthew Rojo <vondgrojo@gmail.com>
2025-12-20 14:06:56 +0800

    Working frontend and backend

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 require('dotenv').config()
 const jwt = require('jsonwebtoken')
+const marked = require('marked')
+const sanitizeHTML = require('sanitize-html')
 const bcrypt = require('bcrypt')
 const cookieParser = require('cookie-parser')
 const express = require('express')
@@ -15,6 +17,17 @@ const createtables = db.transaction(() => {
             password STRING NOT NULL
         )    
     `).run()
+
+    db.prepare(`
+        CREATE TABLE IF NOT EXISTS posts (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            createdDate TEXT,
+            title STRING NOT NULL,
+            body TEXT NOT NULL,
+            authorid INTEGER,
+            FOREIGN KEY (authorid) REFERENCES users(id)
+        )
+    `).run()
 })
 createtables()
 // database setup ends here
@@ -27,6 +40,14 @@ app.use(cookieParser())
 
 // Middleware
 app.use(function (req, res, next) {
+    // make our markdown function available
+    res.locals.filterUserHTML = function (content) {
+        return sanitizeHTML(marked.parse(content), {
+            allowedTags: ['p', 'br', 'ul', 'ol', 'li', 'strong', 'bold', 'em', 'i', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'blockquote', 'a'],
+            allowedAttributed: {}
+        })
+    }
+
     res.locals.errors = []
 
     // try to decode incoming cookie
@@ -44,7 +65,14 @@ app.use(function (req, res, next) {
 
 app.get('/', (req, res) => {
     if (req.user) {
-        return res.render('dashboard')
+        const postsStatement = db.prepare(`
+            SELECT *
+              FROM POSTS
+              WHERE authorid = ?
+              ORDER BY createdDate DESC
+        `)
+        const posts = postsStatement.all(req.user.userid)
+        return res.render('dashboard', { posts })
     }
     res.render('homepage')
 })
@@ -104,8 +132,154 @@ app.post('/login', (req, res) => {
     res.redirect('/')
 })
 
-app.get('/create-post', (req, res) => {
+function mustBeLoggedIn(req, res, next) {
+    if (req.user) {
+        return next()
+    }
+    res.redirect('/')
+}
+
+app.get('/create-post', mustBeLoggedIn, (req, res) => {
     res.render('create-post')
+})
+
+function sharedPostValidation(req) {
+    const errors = []
+
+    if (typeof req.body.title !== 'string') req.body.title = ''
+    if (typeof req.body.body !== 'string') req.body.body = ''
+
+
+    // trim - sanitize or strip out html
+    req.body.title = sanitizeHTML(req.body.title.trim(), { allowedTags: [], allowedAttributes: {} })
+    req.body.body = sanitizeHTML(req.body.body, { allowedTags: [], allowedAttributes: {} })
+
+    if (!req.body.title) errors.push('Title must be provided')
+    if (!req.body.body) errors.push('Content must be provided')
+    
+    return errors
+}
+
+
+app.get('/edit-post/:id', mustBeLoggedIn, (req, res) => {
+    // try to look up the post in question
+    const statement = db.prepare(`
+        SELECT *
+          FROM posts
+          WHERE id = ?
+    `)
+    const post = statement.get(req.params.id)
+
+    if (!post) {
+        return res.redirect('/')
+    }
+
+    // if you're not the author, redirect to homepage
+    if (post.authorid !== req.user.userid) {
+        return res.redirect('/')
+    }
+
+    // otherwise, render edit-post template
+    res.render('edit-post', { post })
+})
+
+app.post('/edit-post/:id', mustBeLoggedIn, (req, res) => {
+    // try to look up the post in question
+    const statement = db.prepare(`
+        SELECT *
+          FROM posts
+          WHERE id = ?
+    `)
+    const post = statement.get(req.params.id)
+
+    if (!post) {
+        return res.redirect('/')
+    }
+
+    // if you're not the author, redirect to homepage
+    if (post.authorid !== req.user.userid) {
+        return res.redirect('/')
+    }
+
+    const errors = sharedPostValidation(req)
+
+    if (errors.length) {
+        return res.render('edit-post', {errors})
+    }
+
+    const updateStatement = db.prepare(`
+        UPDATE posts
+          SET title = ?, body = ?
+          WHERE id = ?
+    `)
+    updateStatement.run(req.body.title, req.body.body, req.params.id)
+
+    res.redirect(`/post/${req.params.id}`)
+})
+
+app.post('/delete-post/:id', mustBeLoggedIn, (req, res) => {
+    // try to look up the post in question
+    const statement = db.prepare(`
+        SELECT *
+          FROM posts
+          WHERE id = ?
+    `)
+    const post = statement.get(req.params.id)
+
+    if (!post) {
+        return res.redirect('/')
+    }
+
+    // if you're not the author, redirect to homepage
+    if (post.authorid !== req.user.userid) {
+        return res.redirect('/')
+    }
+
+    const deleteStatement = db.prepare('DELETE FROM posts WHERE id = ?')
+    deleteStatement.run(req.params.id)
+
+    res.redirect('/')
+})
+
+app.get('/post/:id', (req, res) => {
+    const statement = db.prepare(`
+        SELECT posts.*, users.username
+          FROM posts
+          INNER JOIN users ON posts.authorid = users.id
+          WHERE posts.id = ?
+    `)
+    const post = statement.get(req.params.id)
+
+    if (!post) {
+        return res.redirect('/')
+    }
+    const isAuthor = post.authorid === req.user.userid
+
+    res.render('single-post', { post, isAuthor })
+})
+
+app.post('/create-post', mustBeLoggedIn, (req, res) => {
+    const errors = sharedPostValidation(req)
+
+    if (errors.length) {
+        return res.render('create-post', { errors})
+    }
+    
+    // save post to database
+    const ourStatement = db.prepare(`
+        INSERT INTO posts (title, body, authorid, createdDate)
+        VALUES (?, ?, ?, ?)
+    `)
+    const result = ourStatement.run(req.body.title, req.body.body, req.user.userid, new Date().toISOString())
+
+    const getPostStatement = db.prepare(`
+        SELECT *
+          FROM posts
+          WHERE ROWID = ?
+    `)
+    const realPost = getPostStatement.get(result.lastInsertRowid)
+
+    res.redirect(`/post/${realPost.id}`)
 })
 
 app.post('/register', (req, res) => {
